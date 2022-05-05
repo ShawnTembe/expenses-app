21f796bfe60df39cd40cd53a5c1bef184200f7f2
ShawnTembe1 <shawn.tembe@falcorp.co.za>
2022-05-05 11:01:35 +0200

    first commit

---
+++ src/app/pages/HomePage/HomePage.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { useInjectReducer } from 'utils/redux-injectors';
+import DeafultLayout from 'app/layouts/DefaultLayout';
+import Transactions from './compoenents/Transactions';
+import { reducer, sliceKey } from './slice';
+
+export function HomePage() {
+  useInjectReducer({ key: sliceKey, reducer: reducer });
+  return (
+    <DeafultLayout>
+      <h1>{('HOMPAGE')}</h1>
+      <Transactions />
+    </DeafultLayout>
+  );
+}
